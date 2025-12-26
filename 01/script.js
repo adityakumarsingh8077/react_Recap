b6a5585fffef194eebcc8c81bced44d8554750c3
Aditya Kumar Singh <singhaditya8077@gmail.com>
2025-12-27 04:35:58 +0530

    lec 01 done

--- 01/script.js
+++ 01/script.js
@@ -1,3 +1,29 @@
-const totalApples=10;
 const basket1Count =document.querySelector(".basket1 span");
-basket1Count.innerText=totalApples;
+const basket2Count = document.querySelector(".basket2 span");   
+const rightArrow= document.querySelector(".right-arrow")
+const leftArrow= document.querySelector(".left-arrow")
+
+let totalApples=10;
+let secondBasketApplesCount=0;
+let firstBasketApplesCount=totalApples-secondBasketApplesCount;
+
+basket1Count.innerText= firstBasketApplesCount;
+basket2Count.innerText = secondBasketApplesCount;
+
+rightArrow.addEventListener("click",()=>{
+   if(firstBasketApplesCount>0) {
+    secondBasketApplesCount++;
+    firstBasketApplesCount--;
+    basket1Count.innerText= firstBasketApplesCount;
+    basket2Count.innerText = secondBasketApplesCount;
+    }
+
+})
+leftArrow.addEventListener("click",()=>{
+   if(firstBasketApplesCount>10){
+    secondBasketApplesCount--;
+    firstBasketApplesCount++;
+    basket1Count.innerText= firstBasketApplesCount;
+    basket2Count.innerText = secondBasketApplesCount;
+   }
+})
